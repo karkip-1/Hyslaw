a53d00b5f230549a1c934630322569e4eb31d27e
Prafulla Karki <prafulla_karki@yahoo.com>
2025-01-12 22:49:30 -0500

    Updated to read lb data from github itself

--- hy_law_final.py
+++ hy_law_final.py
@@ -18,8 +18,7 @@ Analyze clinical trial data to identify subjects meeting **Hy's Law criteria**:
 """)
 
 # GitHub CSV URL
-github_csv_url = "https://raw.githubusercontent.com/karkip-1/demo/main/sdtm_lb_data.csv"  # Update with your actual raw GitHub URL
-
+github_csv_url = "https://raw.githubusercontent.com/karkip-1/demo/main/lb.csv"  
 # Read Dataset from GitHub URL
 try:
     df = pd.read_csv(github_csv_url)
